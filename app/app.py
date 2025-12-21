d623f5dffa1592edffd024369aa88514bd8d2a8b
derumesarah <derumesarah@gmail.com>
2025-12-21 18:09:02 -0300

    app.py

--- app/app.py
+++ app/app.py
@@ -26,9 +26,6 @@ if not os.path.exists(MODEL_PATH):
     st.error("Model file not found!")
     st.stop()
 
-model = load_model(MODEL_PATH, os.path.getmtime(MODEL_PATH))
-st.write("TYPE:", type(model))
-st.write("HAS PREDICT:", hasattr(model, "predict"))
 
 st.subheader("🔧 Song Attributes")
 
