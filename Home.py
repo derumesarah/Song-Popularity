62d4c7fe4dee9746aaba9ff8d83aa0f61aaca4e9
derumesarah <derumesarah@gmail.com>
2025-12-22 17:46:01 -0300

    Home Page

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Song Popularity App",
+    layout="wide"
+)
+
+st.title("🎵 Song Popularity Prediction App")
+
+st.markdown("""
+Welcome! 
+This project was build to predict the popularity of a song based on its audio features and platform presence.
+Use the sidebar to navigate between pages:
+- **Predict** – predict streams
+- **Insights** – project insights
+
+""")
