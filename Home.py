c316c458fe3f40182dbda47bbf9c75c65c213af8
derumesarah <derumesarah@gmail.com>
2025-12-22 18:31:10 -0300

    Home Page

--- Home.py
+++ Home.py
@@ -20,5 +20,5 @@ st.info("👉 Use the **sidebar** (left) to switch between pages anytime.")
 st.markdown("### 🧾 What you can do here")
 st.markdown("""
 - **Predict**: Enter song attributes and get an estimated stream count.
-- **Insights**: Learn which patterns and features drive a song’s popularity on streaming platforms
+- **Insights**: Learn which patterns and features drive a song’s popularity on streaming platforms.
 """) 
