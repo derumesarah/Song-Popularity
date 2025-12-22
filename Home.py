6d8ee919ac654dc4597b9cd9a73116e91692ad1f
derumesarah <derumesarah@gmail.com>
2025-12-22 18:28:25 -0300

    Home Page

--- Home.py
+++ Home.py
@@ -6,12 +6,19 @@ st.set_page_config(
 )
 
 st.title("🎵 Song Popularity Prediction App")
+st.caption("Predict estimated stream counts based on song data + audio features.")
 
+st.markdown("---")
+
+st.subheader("👋 Welcome!")
+st.write(
+    "This project was build to help you estimate the expected number of streams for a song 
+)
+
+st.info("👉 Use the **sidebar** (left) to switch between pages anytime.")
+
+st.markdown("### 🧾 What you can do here")
 st.markdown("""
-Welcome! 
-This project was build to predict the popularity of a song based on its audio features and platform presence.
-Use the sidebar to navigate between pages:
-- **Predict** – predict streams
-- **Insights** – project insights
----
-""")
+- **Predict**: Enter song attributes and get an estimated stream count.
+- **Insights**: Learn which patterns and features drive a song’s popularity on streaming platforms
+
