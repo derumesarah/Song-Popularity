d44fb605f4a7294e01b53676066e564ff5e9440d
derumesarah <derumesarah@gmail.com>
2025-12-22 17:25:09 -0300

    Streamlit

--- app/Key Insights.py
+++ app/Key Insights.py
@@ -1,2 +1,11 @@
 import streamlit as st
 from PIL import Image 
+import os
+
+st.set_page_config(page_title="Insights", layout="wide")
+
+st.markdown("Insights
+
+Welcome to the Insights page!
+Here you can find patterns that help to explain what drives a song's popularity on streaming platforms.
+")
