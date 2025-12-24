1e870f5f7dd974ab3d4b4c7978025db2c64d0e99
derumesarah <derumesarah@gmail.com>
2025-12-24 17:17:17 -0300

    Update 2 - Insights.py

--- pages/2 - Insights.py
+++ pages/2 - Insights.py
@@ -4,8 +4,29 @@ import os
 
 st.set_page_config(page_title="Insights", layout="wide")
 
-st.markdown("Insights
+st.title("📊 Insights")
+st.caption("Key findings from exploratory data analysis (EDA) and model interpretation")
 
-Welcome to the Insights page!
-Here you can find patterns that help to explain what drives a song's popularity on streaming platforms.
-")
+st.markdown("""
+This page summarizes the **most important patterns and relationships** found during the exploratory data analysis  
+of the dataset. The goal is to translate data insights into **clear, actionable understanding**.
+""")
+
+st.marksown(---)
+
+# 1. Distribution of streams
+st.subheader("🎧 Overall Distribution of Streams")
+
+st.image("images/Distribution streams.png", use_container_width=True)
+
+st.write("""
+The distribution of Spotify streams is **strongly right-skewed**:
+- Most songs achieve relatively low stream counts
+- A small number of songs reach extremely high values
+
+**Implication:**  
+This large variance justifies the **log-transformation** of streams used during model training,
+as it stabilizes the learning process and prevents overemphasis on extreme outliers.
+""")
+
+st.markdown("---")
